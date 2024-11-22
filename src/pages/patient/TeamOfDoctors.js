9a2b43ed76adfdb75d17f1dc427c9f36193f6b88
haons <haons@kienlongbank.com>
2024-11-23 02:54:50 +0700

    finish

--- src/pages/patient/TeamOfDoctors.js
+++ src/pages/patient/TeamOfDoctors.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import {
+  Container,
   Typography,
-  Paper,
+  Card,
+  CardContent,
   Button,
   Box,
   TextField,
-  Select,
-  MenuItem,
+  Autocomplete,
+  Grid,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
   FormControl,
-  InputLabel,
+  Paper,
   Pagination,
 } from "@mui/material";
 import HeaderComponent from "../../components/patient/HeaderComponent";
@@ -24,18 +29,22 @@ import axios from "axios";
 const TeamOfDoctors = () => {
   const navigate = useNavigate();
   const [selectedTab, setSelectedTab] = useState("Trang chủ");
+  const { provinceSlug, districtSlug } = useParams(); // Lấy tham số từ URL
   const [doctors, setDoctors] = useState([]);
   const [doctorSelected, setDoctorSelected] = useState();
   const [selectedDates, setSelectedDates] = useState({});
   const [isBookingFormOpen, setIsBookingFormOpen] = useState(false);
   const [selectedSchedule, setSelectedSchedule] = useState(null);
   const [selectedDateClick, setSelectedDateClick] = useState();
-    const [selectedProvince, setSelectedProvince] = useState(null); // Lưu tỉnh đã chọn
+  const [selectedProvince, setSelectedProvince] = useState(null); // Lưu tỉnh đã chọn
   const [selectedDistrict, setSelectedDistrict] = useState(null); // Lưu huyện đã chọn
+  const [fullname, setFullName] = useState("");
+  const [sortOrder, setSortOrder] = useState("");
 
   // Thêm trạng thái cho phân trang
   const [currentPage, setCurrentPage] = useState(1);
   const [pageMax, setPageMax] = useState(1);
+  const searchParams = new URLSearchParams();
 
   const today = new Date();
   const maxDate = new Date();
@@ -43,11 +52,44 @@ const TeamOfDoctors = () => {
 
   const [options, setOptions] = useState([]); // State to store city options
 
-
   const getCity = async () => {
-    const data = await axios.get("https://raw.githubusercontent.com/kenzouno1/DiaGioiHanhChinhVN/master/data.json");
-    console.log(data.data)
+    const data = await axios.get(
+      "https://raw.githubusercontent.com/kenzouno1/DiaGioiHanhChinhVN/master/data.json"
+    );
+    console.log(data.data);
     setOptions(data.data); // Set the fetched options
+  };
+
+  useEffect(() => {
+    const getAllDoctor = async () => {
+      try {
+        const data = await doctor.filterDoctor(
+          currentPage,
+          fullname,
+          selectedDistrict && selectedDistrict.Name,
+          selectedProvince && selectedProvince.Name
+        );
+        setDoctors(data.result.doctorResponse);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllDoctor();
+    getCity();
+  }, [provinceSlug, districtSlug]);
+
+  const handleSortChange = (event) => {
+    const order = event.target.value;
+    setSortOrder(order);
+  };
+
+  const handleSelectChange = (type, newValue) => {
+    if (type === "province") {
+      setSelectedProvince(newValue);
+      setSelectedDistrict(null); // Reset huyện khi thay đổi tỉnh
+    } else if (type === "district") {
+      setSelectedDistrict(newValue);
+    }
   };
 
   const handleDateChange = async (doctorId, date) => {
@@ -77,12 +119,12 @@ const TeamOfDoctors = () => {
   const fetchDoctors = async (page) => {
     const response = await doctor.filterDoctor(page);
     console.log(response);
-    setDoctors(response.result.doctorResponse);
-    setPageMax(response.result.pageMax); // Cập nhật tổng số trang
+    setDoctors(response.result?.doctorResponse);
+    setPageMax(response.result?.pageMax); // Cập nhật tổng số trang
 
     // Khởi tạo selectedDates với ngày hiện tại cho tất cả bác sĩ
     const initialSelectedDates = {};
-    response.result.doctorResponse.forEach((doctor) => {
+    response.result?.doctorResponse.forEach((doctor) => {
       const localDate = new Date(today.getTime() + 7 * 60 * 60 * 1000);
       const formattedDate = localDate.toISOString().split("T")[0];
       initialSelectedDates[doctor.id] = formattedDate;
@@ -106,6 +148,39 @@ const TeamOfDoctors = () => {
 
   const handleDoctorClick = (doctor) => {
     navigate(`/doctor/${doctor.id}`); // Điều hướng đến trang DoctorDetail
+  };
+
+  const handleSubmit = async (event) => {
+    if (fullname) {
+      searchParams.append("keyword", fullname); // Thêm query string ?keyword=a
+    }
+    let data;
+    const provinceSlug = selectedProvince
+      ? selectedProvince.Name.toLowerCase().replace(/\s+/g, "-")
+      : "";
+    const districtSlug = selectedDistrict
+      ? selectedDistrict.Name.toLowerCase().replace(/\s+/g, "-")
+      : "";
+    const url = districtSlug
+      ? `/doctor/${provinceSlug}/${districtSlug}?${searchParams.toString()}`
+      : provinceSlug
+      ? `/doctor/${provinceSlug}?${searchParams.toString()}`
+      : `/doctor?${searchParams.toString()}`;
+
+    data = await doctor.filterDoctor(
+      currentPage,
+      fullname,
+      selectedDistrict && selectedDistrict.Name,
+      selectedProvince && selectedProvince.Name
+    );
+    setDoctors(data.result.doctorResponse);
+    // navigate(url);
+    if (sortOrder) {
+      const sorted = [...data.result?.doctorResponse].sort((a, b) => {
+        return sortOrder === "asc" ? a.price - b.price : b.price - a.price;
+      });
+      setDoctors(sorted);
+    }
   };
 
   // Hàm để chuyển trang
@@ -138,107 +213,105 @@ const TeamOfDoctors = () => {
           marginBottom={"0px"}
           alignItems={"center"}
         >
-          <Box>
-            <FormControl
-              sx={{
-                m: 1,
-                minWidth: 180,
-                backgroundColor: "white",
-                borderRadius: "4px",
-                ml: 0,
-              }}
-              size="small"
-            >
-              <InputLabel id="demo-select-small-label">Chuyên khoa</InputLabel>
-              <Select
-                labelId="demo-select-small-label"
-                id="demo-select-small"
-                label="Chuyên khoa"
-              >
-                <MenuItem value={1}>Chuyên khoa 1</MenuItem>
-                <MenuItem value={2}>Chuyên khoa 2</MenuItem>
-                <MenuItem value={3}>Chuyên khoa 3</MenuItem>
-              </Select>
-            </FormControl>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={4}>
+              <Autocomplete
+                value={selectedProvince}
+                onChange={(event, newValue) =>
+                  handleSelectChange("province", newValue)
+                }
+                options={options}
+                getOptionLabel={(option) => option.Name || ""}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Tỉnh thành"
+                    sx={{ backgroundColor: "white" }}
+                  />
+                )}
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <Autocomplete
+                value={selectedDistrict}
+                onChange={(event, newValue) =>
+                  handleSelectChange("district", newValue)
+                }
+                options={(selectedProvince && selectedProvince.Districts) || []}
+                getOptionLabel={(option) => option.Name || ""}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Huyện"
+                    sx={{ backgroundColor: "white" }}
+                  />
+                )}
+                style={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                sx={{
+                  height: "56px",
+                  justifyContent: "center",
+                  flexGrow: 1,
+                  backgroundColor: "white",
+                  borderRadius: "4px",
+                }}
+                variant="outlined"
+                placeholder="Search"
+                onChange={(e) => setFullName(e.target.value)}
+                InputProps={{
+                  startAdornment: (
+                    <Box
+                      style={{
+                        marginRight: "20px",
+                        alignItems: "center",
+                        display: "flex",
+                      }}
+                    >
+                      <SearchIcon style={{ color: "#576064" }} />
+                    </Box>
+                  ),
+                  sx: { height: "56px" },
+                }}
+              />
+            </Grid>
+          </Grid>
+          <FormControl component="fieldset" sx={{ marginLeft: 2 }}>
+            <RadioGroup row value={sortOrder} onChange={handleSortChange}>
+              <FormControlLabel
+                value="asc"
+                control={<Radio />}
+                label="Giá tăng dần"
+              />
+              <FormControlLabel
+                value="desc"
+                control={<Radio />}
+                label="Giá giảm dần"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Box>
 
-            <FormControl
-              sx={{
-                m: 1,
-                minWidth: 160,
-                backgroundColor: "white",
-                borderRadius: "4px",
-              }}
-              size="small"
-            >
-              <InputLabel id="demo-select-small-label">Khu vực</InputLabel>
-              <Select
-                onClick={getCity}
-                labelId="demo-select-small-label"
-                id="demo-select-small"
-                label="Khu vực"
-              >
-                {options.map((option) => ( // Map over options to create MenuItems
-                  <MenuItem key={option.id} value={option.id}>
-                    {option.Name} {/* Adjust according to the structure of your data */}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-
-            <FormControl
-              sx={{
-                m: 1,
-                minWidth: 250,
-                backgroundColor: "white",
-                borderRadius: "4px",
-              }}
-              size="small"
-            >
-              <InputLabel id="demo-select-small-label">Giá</InputLabel>
-              <Select
-                labelId="demo-select-small-label"
-                id="demo-select-small"
-                label="Giá"
-              >
-                <MenuItem value={1}>Dưới 200.000đ</MenuItem>
-                <MenuItem value={2}>Từ 200.000đ - 500.000đ</MenuItem>
-                <MenuItem value={3}>Trên 500.000đ</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-
-          <TextField
-            sx={{
-              height: "40px",
-              justifyContent: "center",
-              flexGrow: 1,
-              backgroundColor: "white",
-              borderRadius: "4px",
-              ml: 1,
+        {/* Di chuyển nút lọc xuống dưới tỉnh thành và huyện */}
+        <Box display={"flex"} justifyContent="center" marginY={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              handleSubmit();
             }}
-            variant="outlined"
-            placeholder="Search"
-            InputProps={{
-              startAdornment: (
-                <Box
-                  style={{
-                    marginRight: "20px",
-                    alignItems: "center",
-                    display: "flex",
-                  }}
-                >
-                  <SearchIcon style={{ color: "#576064" }} />
-                </Box>
-              ),
-              sx: { height: "40px" },
-            }}
-          />
+          >
+            Lọc
+          </Button>
         </Box>
       </Box>
 
       <Box container maxWidth={"1200px"}>
         <Box container spacing={3} maxWidth={"1200px"} align="center">
-          {doctors.map((doctor) => (
+          {doctors?.map((doctor) => (
             <Box
               item
               xs={12}
@@ -257,7 +330,7 @@ const TeamOfDoctors = () => {
                   cursor: "pointer",
                 }}
                 onClick={() => handleDoctorClick(doctor)}
-              > 
+              >
                 <img
                   src={doctor.img || "default_image_url"} // Thay đổi theo cấu trúc dữ liệu của bạn
                   style={{
