da12152b2f087728060fd2f2398902f07c1b7e26
haons <haons@kienlongbank.com>
2024-12-11 23:46:59 +0700

    thay doi url

--- src/pages/doctor/PrescriptionManagement.js
+++ src/pages/doctor/PrescriptionManagement.js
@@ -141,7 +141,7 @@ const Prescription = () => {
       console.log(appointment);
 
       const response = await fetch(
-        `https://grateful-flow-production.up.railway.app/api/v1/appointment/pdf/${appointment.id}?status=${e.target.textContent}`,
+        `https://powerful-motivation.railway.internal/api/v1/appointment/pdf/${appointment.id}?status=${e.target.textContent}`,
         {
           method: "GET",
           headers: {
